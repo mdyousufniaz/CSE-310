f0678e31cb8b744e084a177705001a9d74a3d525
mdyousufniaz <mdyousufniaz@gmail.com>
2025-06-24 00:06:54 +0600

    minor

--- Offlines/Three/Solution/run-script.sh
+++ Offlines/Three/Solution/run-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -i
 
 source ~/.bashrc
-antlr4 C12105125Lexer.g4 C12105125Parser.g4
-javac -cp .:/usr/local/lib/antlr-4.13.2-complete.jar C12105125*.java SymbolTable.java Types.java Main.java 
+antlr4 C2105125Lexer.g4 C2105125Parser.g4
+javac -cp .:/usr/local/lib/antlr-4.13.2-complete.jar C2105125*.java SymbolTable.java Types.java Main.java 
 java -cp .:/usr/local/lib/antlr-4.13.2-complete.jar Main $1
